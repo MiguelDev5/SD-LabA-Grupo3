13bcfd328a5cfb4c4061175dcaedc63e88122f95
MiguelDev5 <migangmedina@gmail.com>
2024-06-26 02:11:37 -0500

    Add: Agregando procedimientos almacenados

--- lab008/Procedimientos/OBTENER_INGENIEROS_POR_IDPROYECTO.sql
+++ lab008/Procedimientos/OBTENER_INGENIEROS_POR_IDPROYECTO.sql
@@ -1,13 +1,13 @@
 CREATE PROCEDURE OBTENER_INGENIEROS_POR_IDPROYECTO(IN ID_PROY INT)
 BEGIN
     SELECT 
-        p.Nombre            AS 'Nombre del proyecto',
-        p.Descripcion       AS 'Descripción',
-        p.FecInicio         AS 'Fecha de inicio',
-        p.FecTermino        AS 'Fecha de finalización',
-        i.Nombres           AS 'Nombre Ing.',
-        i.Especialidad      AS 'Especialidad Ing.',
-        i.Cargo             AS 'Cargo Ing.'
+        p.Nombre            AS 'Nombre',
+        p.Descripcion       AS 'Descripcion',
+        p.FecInicio         AS 'FecInicio',
+        p.FecTermino        AS 'FecTermino',
+        i.Nombres           AS 'NombreIng.',
+        i.Especialidad      AS 'EspecialidadIng.',
+        i.Cargo             AS 'CargoIng.'
     FROM proyecto AS p 
     JOIN asignacion AS a ON a.FIDProy = p.IDProy
     JOIN ingeniero AS i ON i.IDIng = a.FIDIng
--- lab008/Procedimientos/OBTENER_PROYECTOS_POR_IDDEPARTAMENTO.sql
+++ lab008/Procedimientos/OBTENER_PROYECTOS_POR_IDDEPARTAMENTO.sql
@@ -1,12 +1,12 @@
 CREATE PROCEDURE OBTENER_PROYECTOS_POR_IDDEPARTAMENTO(IN ID_DPTO INT)
 BEGIN
     SELECT 
-        p.Nombre            AS 'Nombre del proyecto',
-        p.Descripcion       AS 'Descripción',
-        p.FecInicio         AS 'Fecha de inicio',
-        p.FecTermino        AS 'Fecha de finalización',
-        d.IDDpto	        AS 'Nombre Ing.',
-        d.Nombre		    AS 'Especialidad Ing.',
+        p.Nombre            AS 'Nombre',
+        p.Descripcion       AS 'Descripcion',
+        p.FecInicio         AS 'FecInicio',
+        p.FecTermino        AS 'FecTermino',
+        d.IDDpto	        AS 'IDDpto',
+        d.Nombre		    AS 'Departamento'
     FROM proyecto AS p 
     JOIN departamento AS d ON d.IDDpto = p.FIDDpto
     WHERE p.FIDDpto = ID_DPTO;
